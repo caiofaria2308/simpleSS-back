e436539310438119aec704cbbb9efbb55eb2e92a
Caio de Faria <caiofaria2308@gmail.com>
2023-12-04 12:35:52 -0300

    create provider repository

---
+++ src/infrastructure/database/repository/provider/repository_provider.go
@@ -0,0 +1,72 @@
+package repository
+
+import (
+	entity "main/entity/provider"
+
+	"gorm.io/gorm"
+)
+
+type RepositoryProvider struct {
+	DB *gorm.DB
+}
+
+func (r *RepositoryProvider) Create(provider entity.EntityProvider) (*entity.EntityProvider, error) {
+	err := r.DB.Create(&provider).Error
+	if err != nil {
+		return nil, err
+	}
+	return &provider, nil
+}
+
+func (r *RepositoryProvider) GetAll(store_id string) ([]*entity.EntityProvider, error) {
+	var providers []*entity.EntityProvider
+	err := r.DB.Where("store.id =?", store_id).Joins("Store").Find(&providers).Error
+	if err != nil {
+		return nil, err
+	}
+	return providers, nil
+}
+
+func (r *RepositoryProvider) GetByID(id string) (*entity.EntityProvider, error) {
+	var provider entity.EntityProvider
+	err := r.DB.Where("id =?", id).First(&provider).Error
+	if err != nil {
+		return nil, err
+	}
+	return &provider, nil
+}
+
+func (r *RepositoryProvider) Update(provider entity.EntityProvider) (*entity.EntityProvider, error) {
+	err := r.DB.Save(&provider).Error
+	if err != nil {
+		return nil, err
+	}
+	return &provider, nil
+}
+
+func (r *RepositoryProvider) Delete(id string) error {
+	var provider entity.EntityProvider
+	err := r.DB.Where("id =?", id).Delete(&provider).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RepositoryProvider) SearchByCNPJ(store_id string, cnpj string) (providers []*entity.EntityProvider, err error) {
+	providers, err = r.GetAll(store_id)
+	err = r.DB.Where("cnpj = ?", cnpj).Find(&providers).Error
+	if err != nil {
+		return nil, err
+	}
+	return providers, nil
+}
+
+func (r *RepositoryProvider) SearchByName(store_id string, name string) (providers []*entity.EntityProvider, err error) {
+	providers, err = r.GetAll(store_id)
+	err = r.DB.Where("social_reason LIKE ? OR business_name LIKE ?", "%"+name+"%", "%"+name+"%").Find(&providers).Error
+	if err != nil {
+		return nil, err
+	}
+	return providers, nil
+}
